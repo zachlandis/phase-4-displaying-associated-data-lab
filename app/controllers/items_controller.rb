78907245e5be1cfabe8d0c886295d040ffcde433
Zach <zachlandis91@gmail.com>
2023-06-21 13:12:23 -0600

    commit tree 477904e41dc4cd8602ff9c48ef27f2e47eb3e2ad

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+
+    def index
+        items = Item.all 
+        render json: items, status: :ok, include: :user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user, status: :ok, include: :items
+    end
 end
